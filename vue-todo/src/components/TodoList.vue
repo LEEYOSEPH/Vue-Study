89fde081eab45e3aaf10355e826cf90374099d83
YOSEPHLEE <laksmi0628@gmail.com>
2021-07-09 11:21:30 +0900

    feat: TodoList.vue 삭제버튼 구현
    클릭시 로컬스토리지의 키값과 v-for의 index 값을 받아와서 해당 하는 것들을 삭제

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
       <ul>
-        <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{todoItem}}</li>
+        <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
+          {{todoItem}}
+          <span class="removeBtn" v-on:click="removeTodo(todoItem,index)">
+            <li class="fas fa-trash-alt"></li>
+          </span>
+        </li>
       </ul>
   </div>
 </template>
@@ -13,13 +18,18 @@ export default {
       todoItems: []
     }
   },
+  methods: {
+    removeTodo: function(todoItem,index) {
+      localStorage.removeItem(todoItem)
+      this.todoItems.splice(index,1);
+    }
+  },
   created() {
     //인스턴스 생성되자 마자 실행하는 훅
    if(localStorage.length > 0) {
      for(var i = 0; i<localStorage.length; i++) {
        if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
          this.todoItems.push(localStorage.key(i));
-         console.log(localStorage.key(i));
        }
      }
    }
@@ -29,5 +39,37 @@ export default {
 </script>
 
 <style>
+ul{
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li{
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted{
+  color: #b3adad;
 
-</style>vv
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
