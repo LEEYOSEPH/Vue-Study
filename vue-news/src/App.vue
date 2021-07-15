4229b05b4ecdeebc35260145624d0a1214a891c2
YOSEPHLEE <laksmi0628@gmail.com>
2021-07-15 15:57:47 +0900

    feat : rounter-view를 이용해서 라우팅 컴포넌트 표시

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div id="app">
-    hello
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
-export default {
-  methods: {
-    fetchData() {
-    }
-  }
-}
+export default {};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
