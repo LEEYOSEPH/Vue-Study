10364ce9cfd5bac8a87e796fef703c582aa12f6f
LEEYOSEPH <laksmi0628@gmail.com>
2021-07-16 13:21:41 +0900

    refator : mutations, actions 모듈화

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { fetchNewsList, fetchJobsList, fetchAskList } from "../api/index.js";
+import mutations from "./mutations.js";
+import actions from "./actions.js";
 
 Vue.use(Vuex);
 
@@ -21,44 +22,6 @@ export const store = new Vuex.Store({
       return state.news;
     },
   },
-  mutations: {
-    SET_NEWS(state, news) {
-      state.news = news;
-    },
-    SET_JOBS(state, jobs) {
-      state.jobs = jobs;
-    },
-    SET_ASK(state, ask) {
-      state.ask = ask;
-    },
-  },
-  actions: {
-    FETCH_NEWS({ commit }) {
-      fetchNewsList()
-        .then(({ data }) => {
-          commit("SET_NEWS", data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    FETCH_JOBS({ commit }) {
-      fetchJobsList()
-        .then(({ data }) => {
-          commit("SET_JOBS", data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    FETCH_ASK({ commit }) {
-      fetchAskList()
-        .then(({ data }) => {
-          commit("SET_ASK", data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-  },
+  mutations,
+  actions,
 });
