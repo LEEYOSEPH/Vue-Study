d364b115400a32798f53ac5ac76a5cc2f6e5e56f
LEEYOSEPH <laksmi0628@gmail.com>
2021-07-17 08:53:40 +0900

    feat: router-link 사용

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <p v-for="item in fetchedNews">
+    <p v-for="item in fetchedNews" v-bind:key=item.id>
       <a v-bind:href="item.url">
         {{item.title}}
       </a>
-      <small>{{item.time_ago}} by {{item.user}}</small>
+      <small>
+        {{item.time_ago}} by 
+        <router-link v-bind:to="`/user/${item.user}`">{{item.user}}</router-link>
+      </small>
     </p>
   </div>
 </template>
