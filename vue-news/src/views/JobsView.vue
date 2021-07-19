698ef6e08de24bf02a23d967b01d55b39545523c
YOSEPHLEE <laksmi0628@gmail.com>
2021-07-19 09:41:12 +0900

    feat : AskView, JobsView 스타일링

--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,27 +1,71 @@
 <template>
   <div>
-    <p v-for="job in fetchedJobs">
+    <ul class="jobs-list">
+      <li v-for="job in fetchedJobs" v-bind:key="job.id" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ job.points || 0 }}
+        </div>
+        <div>
+          <p class="jobs-title">
+            <a v-bind:href="job.url">
+              {{ job.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            {{ job.time_ago }} by
+            <a :href="job.url">
+              {{ job.domain }}
+            </a>
+          </small>
+        </div>
+      </li>
+    </ul>
+    <!-- <p v-for="job in fetchedJobs">
       <a :href="job.url">
         {{job.title}}
       </a>
       <small>{{job.time_ago}},{{job.domain}}</small>
-    </p>
+    </p> -->
   </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
   computed: {
-    ...mapGetters([
-      'fetchedJobs'
-    ])
+    ...mapGetters(["fetchedJobs"]),
   },
   created() {
-   this.$store.dispatch('FETCH_JOBS')
+    this.$store.dispatch("FETCH_JOBS");
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.jobs-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.jobs-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
