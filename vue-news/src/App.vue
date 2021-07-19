b4935139b0893981c8611ab8590b2699544f9c30
YOSEPHLEE <laksmi0628@gmail.com>
2021-07-19 09:26:17 +0900

    feat: NewsView 스타일링

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <transition  name="page">
+    <transition name="page">
       <router-view></router-view>
-    </transition >
+    </transition>
   </div>
 </template>
 
@@ -21,10 +21,22 @@ body {
   padding: 0;
   margin: 0;
 }
+a {
+  color: #34495e;
+  text-decoration: none;
+}
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+a.router-link-exact-active {
+  text-decoration: underline;
+}
 
 /* Router transition  */
-.page-enter-active, .page-leave-active {
-  transition: opacity .5s;
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
 }
 .page-enter, .page-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
