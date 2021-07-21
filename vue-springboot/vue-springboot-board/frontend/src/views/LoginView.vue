82b6a9004290eaa0323cf744e77599a632b37f1a
YOSEPHLEE <laksmi0628@gmail.com>
2021-07-21 17:19:50 +0900

    feat: LoginView.vue 생성

--- vue-springboot/vue-springboot-board/frontend/src/views/LoginView.vue
+++ vue-springboot/vue-springboot-board/frontend/src/views/LoginView.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>LOGIN</div>
+  <div>
+    <h2>Log In</h2>
+    <form @submit="onSubmit">
+      <input placeholder="Enter your ID" v-model="uid" />
+      <input placeholder="Enter your password" v-model="password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Login",
+  data: () => ({
+    uid: "",
+    password: "",
+  }),
+  methods: {
+    onSubmit() {
+      console.log(this.uid);
+      console.log(this.password);
+    },
+  },
+};
 </script>
-
-<style></style>
