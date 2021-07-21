20fd02c58217c8477bfd6d911397fd853fe1ba2d
YOSEPHLEE <laksmi0628@gmail.com>
2021-07-21 10:50:36 +0900

    feat: vue-springboot 프로젝트 생성

---
+++ vue-springboot/vue-springboot-board/src/test/java/com/example/demo/VueSpringbootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSpringbootBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
