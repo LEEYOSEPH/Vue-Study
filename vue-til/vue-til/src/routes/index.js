c25b2817ad957ed1f080bf943c5f10425b4b8b6f
YOSEPHLEE <laksmi0628@gmail.com>
2021-07-22 10:48:02 +0900

    feat: vue-router redirect 설정
    LoginPage로 이동

--- vue-til/vue-til/src/routes/index.js
+++ vue-til/vue-til/src/routes/index.js
@@ -1,18 +1,22 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import LoginPage from '@/views/LoginPage.vue';
-import SignupPage from '@/views/SignupPage.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import LoginPage from "@/views/LoginPage.vue";
+import SignupPage from "@/views/SignupPage.vue";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
   routes: [
     {
-      path: '/login',
+      path: "/",
+      redirect: LoginPage,
+    },
+    {
+      path: "/login",
       component: LoginPage,
     },
     {
-      path: 'signup',
+      path: "signup",
       component: SignupPage,
     },
   ],
