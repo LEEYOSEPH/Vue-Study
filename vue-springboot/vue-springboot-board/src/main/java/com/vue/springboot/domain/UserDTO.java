3da9b7e688240a46e30f1f1a6695ec37022f9659
YOSEPHLEE <laksmi0628@gmail.com>
2021-07-23 17:51:47 +0900

    fix: userDTO  재설정

--- vue-springboot/vue-springboot-board/src/main/java/com/vue/springboot/domain/UserDTO.java
+++ vue-springboot/vue-springboot-board/src/main/java/com/vue/springboot/domain/UserDTO.java
@@ -1,23 +1,12 @@
 package com.vue.springboot.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
 public class UserDTO {
 	
-	//아이디
 	private String username;
-	
-	//비밀번호
 	private String password;
-	
-	//닉네임
 	private String nickname;
+
 }
