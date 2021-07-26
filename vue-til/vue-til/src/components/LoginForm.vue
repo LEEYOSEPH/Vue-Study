f47eadf27c649e849c2e45739e1713a02f3d89a8
YOSEPHLEE <laksmi0628@gmail.com>
2021-07-26 10:29:56 +0900

    feat: 로그인 기능 구현

--- vue-til/vue-til/src/components/LoginForm.vue
+++ vue-til/vue-til/src/components/LoginForm.vue
@@ -1,19 +1,37 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div>
       <label for="username">id:</label>
-      <input id="username" type="text" />
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">pw:</label>
-      <input id="password" type="text" />
+      <input id="password" type="text" v-model="password" />
     </div>
     <button type="submit">로그인</button>
   </form>
 </template>
 
 <script>
-export default {};
+import { loginUser } from "../api/index";
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    async submitForm() {
+      const userData = {
+        username: this.username,
+        password: this.password,
+      };
+      const response = await loginUser(userData);
+      console.log(response);
+    },
+  },
+};
 </script>
 
 <style></style>
