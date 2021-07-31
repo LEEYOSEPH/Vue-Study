9f36a8442a2f5341826a2267e49d1e97dd9d82b2
LEEYOSEPH <laksmi0628@gmail.com>
2021-07-31 11:45:18 +0900

    feat: 인증되지 않은 사용자 접근 제어

--- vue-til/vue-til/src/routes/index.js
+++ vue-til/vue-til/src/routes/index.js
@@ -1,38 +1,59 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import store from '../store/index';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
-  mode: "history",
+const router = new VueRouter({
+  mode: 'history',
   routes: [
     {
-      path: "/",
-      redirect: "/login",
+      path: '/',
+      redirect: '/login',
     },
     {
-      path: "/login",
-      component: () => import("@/views/LoginPage.vue"),
+      path: '/login',
+      component: () => import('@/views/LoginPage.vue'),
     },
     {
-      path: "/signup",
-      component: () => import("@/views/SignupPage.vue"),
+      path: '/signup',
+      component: () => import('@/views/SignupPage.vue'),
     },
     {
-      path: "/main",
-      component: () => import("@/views/MainPage.vue"),
+      path: '/main',
+      component: () => import('@/views/MainPage.vue'),
+      meta: {
+        auth: true,
+      },
     },
     {
-      path: "/add",
-      component: () => import("@/views/PostAddPage.vue"),
+      path: '/add',
+      component: () => import('@/views/PostAddPage.vue'),
+      meta: {
+        auth: true,
+      },
     },
     {
-      path: "/post/:id",
-      component: () => import("@/views/PostEditpage.vue"),
+      path: '/post/:id',
+      component: () => import('@/views/PostEditpage.vue'),
+      meta: {
+        auth: true,
+      },
     },
     {
-      path: "*",
-      component: () => import("@/views/NotFoundPage.vue"),
+      path: '*',
+      component: () => import('@/views/NotFoundPage.vue'),
     },
   ],
 });
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !store.getters.isLogin) {
+    console.log('인증이 필요합니다.');
+    next('/login');
+    //next 타고 밑의 넥스트가 실행 안된다.
+    return;
+  }
+  next();
+});
+
+export default router;
