7f5e12cd2845a08d8089130cdba60a7d2f7005f4
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-04 10:26:48 +0900

    feat: MainPage 컴포넌트 글쓰기 버튼 생성
    로그인을 한경우에는 글쓰기 버튼이 보이고 로그인 하지 않는 경우에는 버튼이 보이지 않도록 했습니다.

--- vue-springboot/vue-springboot-board/frontend/src/views/MainPage.vue
+++ vue-springboot/vue-springboot-board/frontend/src/views/MainPage.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>Main</div>
+  <div>
+    <div class="main list-container contents"></div>
+
+    <router-link to="/add" class="create-button" v-if="!isLogin">
+      <i class="ion-md-add"></i>
+    </router-link>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    isLogin() {
+      return this.$store.getters.isLogin;
+    },
+  },
+};
 </script>
 
 <style></style>
