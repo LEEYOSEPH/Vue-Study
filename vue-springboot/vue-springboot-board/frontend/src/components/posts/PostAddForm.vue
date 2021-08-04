605db59b264daa248a751389d083aa091a3bc531
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-04 11:16:39 +0900

    feat: 토스으 에디터 컴포넌트 분리
    게시글 작성 컴포넌트에서 토스트데이터작성 컴포넌트를 분리 했습니다.

--- vue-springboot/vue-springboot-board/frontend/src/components/posts/PostAddForm.vue
+++ vue-springboot/vue-springboot-board/frontend/src/components/posts/PostAddForm.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>게시글 작성 폼</div>
+  <div class="contents">
+    <h1 class="page-header">Create Post</h1>
+    <div class="form-wrapper">
+      <form class="form" @submit.prevent="submitForm">
+        <div>
+          <label for="title">Title </label>
+          <input id="title" type="text" v-model="title" />
+        </div>
+        <div>
+          <toast-editor></toast-editor>
+        </div>
+        <button type="submit" class="btn">Create</button>
+      </form>
+      <p class="log">
+        {{ logMessage }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import ToastEditor from '../utils/ToastEditor.vue';
+
+export default {
+  components: { ToastEditor },
+  
+};
 </script>
 
-<style></style>
+<style scoped>
+.form {
+  width: 100%;
+  height: 100%;
+}
---
+++ vue-springboot/vue-springboot-board/frontend/src/components/utils/ToastEditor.vue
@@ -0,0 +1,16 @@
+<template>
+  <editor></editor>
+</template>
+
+<script>
+import "codemirror/lib/codemirror.css";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/vue-editor";
+export default {
+  components: {
+    Editor,
+  },
+};
+</script>
+
+<style></style>
