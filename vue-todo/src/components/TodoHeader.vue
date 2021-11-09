7ff51f7f3630d6e2d08a45b650831198f3b9690d
LEEYOSEPH <laksmi0628@gmail.com>
2021-11-09 21:48:20 +0900

    feat: TodoHeader, App components CSS

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,14 @@
 <template>
-  <div>
-      header
-  </div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
-<script>
-export default {
 
+<style scoped>
+h1{
+  color: #2f3b52;
+  font-weight: 900;
+   margin: 2.5rem 0 1.5rem; /* 바깥여백 */
 }
-</script>
-
-<style>
-
 </style>
