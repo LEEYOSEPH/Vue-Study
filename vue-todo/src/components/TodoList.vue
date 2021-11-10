23ada5121f4d687b440fc73a4b9473f069eed688
LEEYOSEPH <laksmi0628@gmail.com>
2021-11-10 21:55:29 +0900

    feat: TodoList Component UI 스타일링

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
       <ul>
-        <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{todoItem}}</li>
+        <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
+          {{todoItem}}
+          <span class="removeBtn" v-on:click="removeTodo">
+            <i class="fas fa-trash-alt"></i>
+          </span>
+        </li>
       </ul>
   </div>
 </template>
@@ -13,10 +18,17 @@ export default {
       todoItems: []
     }
   },
+  methods:{
+    removeTodo: function() {
+      
+    }
+  },
   created: function() {
    if(localStorage.length> 0) {
      for(let i = 0; i< localStorage.length; i++) {
-       this.todoItems.push(localStorage.key(i));
+       if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+         this.todoItems.push(localStorage.key(i));
+       }
      }
    }
   }
@@ -24,6 +36,37 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+ul{
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: center;
+}
+li{
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn{
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted{
+  color: #b3adad;
+}
+.textCompleted{
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn{
+  margin-left: auto;
+  color: #de4343;
+}
 </style>
